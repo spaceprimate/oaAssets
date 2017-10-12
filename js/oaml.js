c935c75c1b5ddd09173796158568adb5785af7f1
Daniel Murphy <dtmurph1@uno.edu>
2017-10-12 13:38:17 -0500

    js

--- js/oaml.js
+++ js/oaml.js
@@ -516,6 +516,20 @@ function arrayHasString(arr, str) {
     return false;
 }
 
+function verticalAlign(id){
+    $(id).css({
+        'position': 'absolute',
+        'left': '50%',
+        'top': '50%',
+        'margin-left': function () {
+            return -$(this).outerWidth() / 2
+        },
+        'margin-top': function () {
+            return -$(this).outerHeight() / 2
+        }
+    });
+}
+
 /**
  * Even if content doesn't fill up the sreen, make sure the footer is still on the bottom of the screen. 
  */
